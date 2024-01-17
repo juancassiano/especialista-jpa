52fa318e0046736b9a94e258deb4501c1f188a68
juancassiano <juancassiano@hotmail.com>
2024-01-17 14:02:32 -0300

    "feat": Estados e Ciclos de Vida de Objetos

---
+++ src/test/java/com/algaworks/ecommerce/conhecendoEntityManager/EstadosECiclosDeVidaTest.java
@@ -0,0 +1,20 @@
+package com.algaworks.ecommerce.conhecendoEntityManager;
+
+import com.algaworks.ecommerce.iniciandocomjpa.EntityManagerTest;
+import com.algaworks.ecommerce.model.Categoria;
+import org.junit.Test;
+
+public class EstadosECiclosDeVidaTest extends EntityManagerTest {
+
+    @Test
+    public void analisarEstados(){
+        Categoria categoriaNovo = new Categoria(); //Categoria Nova
+        Categoria categoriaGerenciadaMerge = entityManager.merge(categoriaNovo); //Passa a Ser Gerenciada
+        Categoria categoriaGerenciada = entityManager.find(Categoria.class, 1);
+
+        entityManager.remove(categoriaGerenciada); //Removido
+        entityManager.persist(categoriaGerenciada); // Volta a ser Gerenciado
+
+        entityManager.detach(categoriaGerenciada); // Desanexar Categoria
+    }
+}

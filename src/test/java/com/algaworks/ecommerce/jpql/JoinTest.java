7b898269eb4989f617096422fc7de90f568540fd
juancassiano <juancassiano@hotmail.com>
2024-03-03 17:26:07 -0300

    "feat": Usando Join Fetch

--- src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
@@ -11,6 +11,20 @@ import java.util.List;
 public class JoinTest extends EntityManagerTest {
 
     @Test
+    public void usarJoinFetch(){
+        String jpql = "select p from Pedido p " +
+                "left join fetch p.pagamento " +
+                "join fetch p.cliente " +
+                "left join fetch p.notaFiscal " +
+                "where p.id = 1";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+
+        Pedido pedido= typedQuery.getSingleResult();
+        Assert.assertFalse(pedido.getItens().isEmpty());
+    }
+
+    @Test
     public void fazerLeftJoin(){
         String jpql = "select p from Pedido p left join p.pagamento pag on pag.status='PROCESSANDO'";
 

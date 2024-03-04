a6d8d2cc864e22cfb9b39d279ccf23a85b1fc887
juancassiano <juancassiano@hotmail.com>
2024-03-03 22:15:25 -0300

    "feat": Usando Path Expressions

--- src/test/java/com/algaworks/ecommerce/jpql/PathExpressionsTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/PathExpressionsTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,17 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class PathExpressionsTest extends EntityManagerTest {
+
+    @Test
+    public void buscarPedidosComProdutoEspecifico(){
+        String jpql = "select p from Pedido p " +
+                "join p.itens i where i.id.produtoId = 1";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql,Pedido.class);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertTrue(lista.size() == 2);
+
+    }
 
     @Test
     public void usarPathExpressions(){
